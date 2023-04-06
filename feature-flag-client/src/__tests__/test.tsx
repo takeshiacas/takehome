e3594f0093ae469a72bee06601f8860f7ee3da49
onedev111 <developeruser111@gmail.com>
2023-04-07 02:42:50 +0330

    complete the test

--- feature-flag-client/src/__tests__/test.tsx
+++ feature-flag-client/src/__tests__/test.tsx
@@ -2,57 +2,61 @@ import { render, waitFor } from "@testing-library/react";
 import FeatureFlagContextProvider from "../context/feature-flag-context";
 import { MedicationDetails } from "../components/medication-details";
 import { PageLayout } from "../components/page-layout";
-import { RequestReviewButton } from "../components/request-review-button";
 import { getUserNameFromLocalStorage } from "../utils/localStorage";
 import "@testing-library/jest-dom/extend-expect";
-import LaunchDarkly from "../LaunchDarkly";
 
+//At first we mock the LaunchDarkly module so that we can control the flag values
 jest.mock("../LaunchDarkly", () => {
-  const mockLaunchDarkly = {
-    initializeClient: jest.fn(),
-    getInstance: jest.fn().mockReturnValue({
-      initializeClient: jest.fn(),
-      getFlag: jest.fn(),
-    }),
+  let mockFlags: { [key: string]: any } = {
+    "general-render-launch-banner": true,
+    "profile-render-details-section": "variation",
+    "details-section-cta-colour": "blue",
   };
-  return mockLaunchDarkly;
+  const mockInitializeClient = jest.fn();
+
+  const mockInstance = {
+    initializeClient: mockInitializeClient,
+    getFlag: (flagKey: string) => {
+      return mockFlags[flagKey];
+    },
+  };
+
+  const mockGetInstance = jest.fn(() => mockInstance);
+
+  const mockLD = {
+    getInstance: mockGetInstance,
+  };
+  return mockLD;
 });
+
+//We also mock the localStorage module so that we can control the username
 jest.mock("../utils/localStorage", () => {
   return {
     getUserNameFromLocalStorage: jest.fn(),
   };
 });
-jest.mock("launchdarkly-js-client-sdk", () => {
-  const ldClient = {
-    on: jest.fn(),
-  };
-  return {
-    initialize: () => ldClient,
-  };
-});
 
 test("renders dashboard components based on flag values", async () => {
+  //We mock the getUserNameFromLocalStorage function so that we can control the username
   getUserNameFromLocalStorage.mockImplementation(() => {
     return "test";
   });
 
-  // const mockInitializeClient = LaunchDarkly.getInstance()
-  //   .initializeClient as jest.Mock;
-  // mockInitializeClient.mockImplementation(() => {
-  //   return Promise.resolve();
-  // });
-
+  //We render the dashboard screen and wait for the components to be rendered
   const { getByTestId } = render(
     <FeatureFlagContextProvider>
       <PageLayout className="">
         <MedicationDetails />
-        <RequestReviewButton />
       </PageLayout>
     </FeatureFlagContextProvider>
   );
 
+  //We use the waitFor method to wait for the components to be rendered and then we assert that the components are rendered based on the flag values
   await waitFor(() => {
     expect(getByTestId("medication-details")).toBeInTheDocument();
     expect(getByTestId("promotional-banner")).toBeInTheDocument();
+    expect(getByTestId("request-review-button")).toHaveStyle(
+      "background-color: blue"
+    );
   });
 });
